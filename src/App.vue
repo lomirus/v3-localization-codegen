5be59b8b1f959087cf8c51c6d56205329eb96029
Lomirus <lomirus@qq.com>
2023-05-10 15:31:09 +0800

    Add custom title

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ import { message } from '@tauri-apps/api/dialog';
 
 const smText = ref('');
 const enText = ref('');
+const title = ref('')
 
 listen('import', async () => {
   const file = await selectFile();
@@ -22,23 +23,31 @@ async function copyText(text: string) {
 </script>
 
 <template>
+  <input class="custom-title" placeholder="自定义标题..." v-model="title">
   <main>
-    <p>l_simp_chinese.yml</p>
-    <p>l_english.yml</p>
+    <p>l{{ title === '' ? '_' : `_${title}_` }}simp_chinese.yml</p>
+    <p>l{{ title === '' ? '_' : `_${title}_` }}english.yml</p>
     <textarea v-model="smText" spellcheck="false" placeholder="请于菜单栏中导入所需文本表格..." wrap="off"></textarea>
     <textarea v-model="enText" spellcheck="false" placeholder="请于菜单栏中导入所需文本表格..." wrap="off"></textarea>
     <div class="buttons">
       <button class="copy" @click="copyText(smText)">复制</button>
-      <button class="download" @click="saveTextFile('l_simp_chinese', smText)">下载</button>
+      <button class="download"
+        @click="saveTextFile(`l${title === '' ? '_' : `_${title}_`}simp_chinese`, smText)">下载</button>
     </div>
     <div class="buttons">
       <button class="copy" @click="copyText(enText)">复制</button>
-      <button class="download" @click="saveTextFile('l_english', enText)">下载</button>
+      <button class="download" @click="saveTextFile(`l${title === '' ? `_${title}_` : '_'}english`, enText)">下载</button>
     </div>
   </main>
 </template>
 
 <style scoped lang="less">
+.custom-title {
+  display: block;
+  margin: 12px auto 0;
+  width: 180px;
+}
+
 main {
   display: grid;
   grid-template-columns: 1fr 1fr;
@@ -48,7 +57,7 @@ main {
   row-gap: 16px;
   padding: 16px;
   width: 100vw;
-  height: 100vh;
+  height: calc(100vh - 33px);
 
 
   p {
